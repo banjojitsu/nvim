96e878c9545a874f74d2d3eb314146364623ee2f
jason <105950172+banjojitsu@users.noreply.github.com>
2024-10-26 18:52:47 -0400

    update keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,7 +1,7 @@
 local map = vim.keymap.set
 
 map("n", "<leader>f", ":lua MiniFiles.open(vim.api.nvim_buf_get_name(0), false)<cr>", {desc = "files"})
-map("n", "<leader>q", ":q<cr>", {desc = "quit"})
+map("n", "<leader>q", ":q!<cr>", {desc = "quit"})
 map("n", "<leader>w", ":w<cr>", {desc = "write"})
 map("n", "<esc>", ":noh<cr>")
 map("n", "<leader>s", "/", {desc = "search"})
